af69edc11b28bc3bae79d2882462a5a7131a0f18
tvidal02 <thiago_acvidal@hotmail.com>
2015-04-21 19:35:38 -0300

    Create Concessionaria.cs

---
+++ Concessionaria.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LINQ_Joins
+{
+    public class Concessionaria
+    {
+        public string Localizacao { get; set; }
+
+        //registro (a concessionária possui o carro?)
+        public int ConcessionariaId { get; set; }
+    }
+}
